89939d61ef3e215f0f021f5b0468f11e22c0ff72
JuanGTZ07 <al45677@un.edu.mx>
2025-11-18 19:37:06 -0600

    final

--- JavaScript.js
+++ JavaScript.js
@@ -6,6 +6,8 @@ function calcular() {
     var preciomarca = Number(marca.value);
     var nombre = document.getElementById("nombre").value;
     var apellido = document.getElementById("apellido").value;
+    var finan = formulario.finan.value;
+    var seguro = Number(document.getElementById("seguro").value);
 
     document.querySelectorAll('input[type="checkbox"]').forEach(extra => {
         if(extra.checked) {
@@ -14,6 +16,22 @@ function calcular() {
     })
 
     total += preciomarca;
+
+    total += total *0.16;
+
+    if (finan == "credito") {
+        interes = total *0.19
+        total += interes
+        document.getElementById('interes').textContent = "Interes: $" + interes.toLocaleString();
+    }
+    else if (finan == "contado") {
+        document.getElementById('interes').textContent = "";
+    }
+
+    if (seguro == "12750") {
+        document.getElementById('SEGURO').innerText = "Seguro anual: $" + seguro.toLocaleString();
+    }
+
     document.getElementById('NA').innerText = `${nombre} ${apellido}`
     document.getElementById('COT').innerText = "total $" + total.toLocaleString();
 }
